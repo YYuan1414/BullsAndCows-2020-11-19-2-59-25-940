f585a3e7b920932e1e3dbc27b928dcf35ebc1786
YYuan1414 <yuanyiren19900728@163.com>
2020-11-19 16:05:51 +0800

    test: return 0A0B test

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -5,16 +5,33 @@ namespace BullsAndCows
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private readonly string secret = string.Empty;
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            this.secret = this.secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            throw new NotImplementedException();
+            return Compare(this.secret, guess);
+        }
+
+        private string Compare(string secret, string guess)
+        {
+            //int equalNumber = 0;
+            //
+            //foreach (var digital in secret)
+            //{
+            //    if (guess.Contains(digital))
+            //    {
+            //        equalNumber++;
+            //    }
+            //}
+            //for (position=0;p)
+            return "0A0B";
         }
     }
 }
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -8,10 +8,28 @@ namespace BullsAndCowsTest
         [Fact]
         public void Should_create_BullsAndCowsGame()
         {
-            var secretGenerator = new SecretGenerator();
-            var game = new BullsAndCowsGame(secretGenerator);
+            var testSecretGenerator = new TestSecretGernerate();
+            var game = new BullsAndCowsGame(testSecretGenerator);
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
+        }
+
+        [Fact]
+        public void Should_create_0A0B_When_All_Digital_And_Position_Wrong()
+        {
+            var testSecretGenerator = new TestSecretGernerate();
+            var game = new BullsAndCowsGame(testSecretGenerator);
+            string answer = game.Guess("5678");
+
+            Assert.Equal($"0A0B", answer);
+        }
+    }
+
+    public class TestSecretGernerate : SecretGenerator
+    {
+        public override string GenerateSecret()
+        {
+            return "1234";
         }
     }
 }
