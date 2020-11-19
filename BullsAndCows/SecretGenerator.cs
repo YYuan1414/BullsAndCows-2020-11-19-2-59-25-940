a43f6aea9b2538dd885acdf1ac5d5481168055be
YYuan1414 <yuanyiren19900728@163.com>
2020-11-19 21:32:18 +0800

    faet: modify the input valid

--- BullsAndCows/SecretGenerator.cs
+++ BullsAndCows/SecretGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -9,15 +10,17 @@ namespace BullsAndCows
     {
         public virtual string GenerateSecret()
         {
-            //Regex secret = new Regex("/^[0 - 9]{3,}[0 - 9]$/");
-            //do
-            //{
-            //    secret = new Regex("/^[0 - 9]{3,}[0 - 9]$/");
-            //}
-            //while (secret.ToString().ToList().Distinct().Count() != secret.ToString().Length);
+            List<string> secretSourceList = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            var random = new Random();
+            string secret = string.Empty;
+            for (var i = 0; i < 4; i++)
+            {
+                int index = random.Next(0, secretSourceList.Count);
+                secret += secretSourceList[index];
+                secretSourceList.RemoveAt(index);
+            }
 
-            //return secret.ToString();
-            return string.Empty;
+            return secret;
         }
     }
 }
--- BullsAndCowsRunner/Program.cs
+++ BullsAndCowsRunner/Program.cs
@@ -8,14 +8,20 @@ namespace BullsAndCowsRunner
         public static void Main(string[] args)
         {
             SecretGenerator secretGenerator = new SecretGenerator();
-            //CheckValid checkValid = new CheckValid();
+            CheckValid checkValid = new CheckValid();
             BullsAndCowsGame game = new BullsAndCowsGame(secretGenerator);
             while (game.CanContinue)
             {
                 var input = Console.ReadLine();
-
-                var output = game.Guess(input);
-                Console.WriteLine(output);
+                if (checkValid.IsValid(input))
+                {
+                    var output = game.Guess(input);
+                    Console.WriteLine(output);
+                }
+                else
+                {
+                    continue;
+                }
             }
 
             Console.WriteLine("Game Over");
