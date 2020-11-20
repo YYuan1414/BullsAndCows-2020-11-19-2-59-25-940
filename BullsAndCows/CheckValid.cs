2d2bd7f905cbd756d889aa217a3d90063771cdeb
YYuan1414 <yuanyiren19900728@163.com>
2020-11-20 08:48:35 +0800

    feat: add checkValid class

---
+++ BullsAndCows/CheckValid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BullsAndCows
+{
+    public class CheckValid
+    {
+        public bool IsValid(string guess)
+        {
+            Regex vaildFormat = new Regex(@"^(\d\s){3}\d$");
+            return vaildFormat.IsMatch(guess) && (guess.Split(' ').ToList().Distinct().Count() == 4);
+        }
+    }
+}
